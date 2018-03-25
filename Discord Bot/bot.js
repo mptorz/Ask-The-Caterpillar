16ab27cb9b58879e5b7883e113c845883ae6b548
Mikolaj Torz <mptorz@icloud.com>
2018-03-25 00:22:36 +0000

    Added a Discord bot

---
+++ Discord Bot/bot.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const request = require('request');
+
+client.on("ready", () => {
+    console.log("Bot is ready!");
+});
+
+client.on("message", (message) => {
+    let userQuery = message.content;
+    if (userQuery.startsWith("caterpillar") && !message.author.bot) {
+        apiHandler(userQuery.substr(11)).then((response) => {
+            message.channel.send(response);
+        });
+    } else if (message.channel.type === "dm" && !message.author.bot) {
+        apiHandler(userQuery).then((response) => {
+            message.channel.send(response);
+        });
+    }
+});
+
+
+function apiHandler(query) {
+    return new Promise((resolve, reject) => {
+        request.post(
+            {url: 'https://www.askthecaterpillar.com/query', form: {query: query}},
+            function (error, httpResponse, body) {
+                if (error) {
+                    console.log(error);
+                    reject(error);
+                }
+                console.log(body);
+                let parsedBody = JSON.parse(body);
+                let toReturn = parsedBody["data"]["messages"][0]["content"];
+                console.log("api returned: " + toReturn);
+                resolve(toReturn);
+            });
+    });
+}
+
+client.login("SECRETKEY");
